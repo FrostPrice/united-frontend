59c4e0a5c0ba6dba3049c72f6bcc1738337329c8
Jonathas Meine <jonhy.meine@gmail.com>
2024-05-18 14:10:05 -0300

    feat: add individual grades to each average

--- src/components/grade/CourseOverview.jsx
+++ src/components/grade/CourseOverview.jsx
@@ -1,9 +1,39 @@
+import {useState} from 'react';
 import '../../index.css';
-import GradeAverage from './GradeAverage';
+import Grade from './Grade';
 
 function CourseOverview({course}) {
-    const {title, professor, absenceCount, maxAbsence, avgGrades} = course;
+    const {title, professor, absenceCount, maxAbsence, grades} = course;
     const fixedAbsenceCount = absenceCount.toString().padStart(2, '0');
+
+    // Calculate average grades
+    const avgGrades = grades.map(grade => {
+        let finished = true;
+        const sum = grade.reduce((acc, {grade, weight}) => {
+            if (grade === -1) {
+                finished = false;
+                return;
+            }
+            return acc + grade * weight;
+        }, 0);
+        if (!finished) {
+            return -1;
+        }
+        const totalWeight = grade.reduce((acc, {weight}) => acc + weight, 0);
+        return parseFloat((sum / totalWeight).toFixed(1));
+    });
+
+    // Check if all grades are finished
+    let finalAvgFinished = true;
+    if (avgGrades.includes(-1)) {
+        finalAvgFinished = false;
+    }
+    let finalAvg = -1;
+    if (finalAvgFinished) {
+        finalAvg = (avgGrades.reduce((acc, grade) => acc + grade, 0) / avgGrades.length).toFixed(1);
+    }
+
+    const [active, setActive] = useState(false);
 
     return (
         <div className="flex flex-col gap-4 bg-white rounded-md border-2 border-zinc-300 px-4 py-2">
@@ -13,28 +43,38 @@ function CourseOverview({course}) {
                     <p className="text-sm text-zinc-500">{professor}</p>
                     <p className="text-sm text-zinc-500">{`Faltas: ${fixedAbsenceCount} / ${maxAbsence}`}</p>
                 </div>
-                <button className="flex items-center text-sm">
+                <button className="flex items-center text-sm" onClick={() => setActive(!active)}>
                     <span>Detalhes</span>
-                    <span className="material-symbols-rounded">keyboard_arrow_down</span>
+                    <span className="material-symbols-rounded">
+                        {active ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+                    </span>
                 </button>
             </div>
             <div className="flex bg-zinc-300">
-                <div className="w-2/3 flex justify-evenly">
+                <div className="w-2/3 flex">
                     {avgGrades.map((grade, index) => (
-                        <GradeAverage key={index} name={`M${index + 1}`} grade={grade} />
+                        <div key={index} className="grow">
+                            <Grade key={index} name={`M${index + 1}`} grade={grade} />
+                        </div>
                     ))}
                 </div>
                 <div className="w-1/3">
-                    <GradeAverage
-                        name="Média Final"
-                        grade={(
-                            avgGrades.reduce((acc, grade) => {
-                                if (grade === '-') return acc;
-                                return acc + grade;
-                            }, 0) / avgGrades.filter(grade => grade !== '-').length
-                        ).toFixed(1)}
-                    />
+                    <Grade name="Média Final" grade={finalAvg} />
                 </div>
+            </div>
+            <div className={`${active ? 'flex' : 'hidden'} flex-col gap-4 bg-zinc-300 p-2`}>
+                {grades.map((grade, index) => (
+                    <div key={`a${index}`} className="flex">
+                        <span key={`b${index}`} className="w-12 font-bold text-center content-center">{`M${
+                            index + 1
+                        }`}</span>
+                        <div key={`c${index}`} className="grid grid-cols-3 gap-4 grow">
+                            {grade.map(({title, grade, weight}, index) => (
+                                <Grade key={index} name={title} grade={grade} weight={weight.toFixed(1)} />
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/components/grade/Grade.jsx
+++ src/components/grade/Grade.jsx
@@ -1,9 +1,10 @@
 import '../../index.css';
 
-function GradeAverage({name, grade}) {
+function Grade({name, grade, weight}) {
     let color;
-    if (grade === '-') {
+    if (grade == -1) {
         color = 'text-zinc-500';
+        grade = '-';
     } else if (grade >= 6) {
         color = 'text-green-500';
     } else {
@@ -12,10 +13,11 @@ function GradeAverage({name, grade}) {
 
     return (
         <div className="flex flex-col">
-            <span className="text-lg text-center font-bold">{name}</span>
+            <span className="text-lg text-center font-bold line-clamp-3">{name}</span>
             <span className={`text-lg text-center font-bold ${color}`}>{grade}</span>
+            {weight && <span className="text-sm text-center">{`Peso: ${weight}`}</span>}
         </div>
     );
 }
 
-export default GradeAverage;
+export default Grade;
