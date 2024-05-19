dde60e0c69162ddcd49881a5d1a88321de2885b6
mateusbarbosa <barbosamat16@gmail.com>
2024-05-18 21:04:46 -0300

    Feat: Add user store the main store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notificationReducer from "./notificationSlice";
 import authReducer from "./authSlice";
+import userReducer from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     notifications: notificationReducer,
+    user: userReducer,
   },
 });
