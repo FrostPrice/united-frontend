3ac9864aae6daf34df33a2607f3343622349e06c
Mateus Barbosa <barbosamat16@gmail.com>
2024-06-05 18:59:22 -0300

    feat: Add subjects, professors, contents, assessments, and grades slices to store

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,11 @@ import notificationReducer from "./notificationSlice";
 import authReducer from "./authSlice";
 import userReducer from "./userSlice";
 import eventsReducer from "./eventsSlice";
+import subjectsReducer from "./subjectSlice";
+import professorsReducer from "./professorSlice";
+import contentsReducer from "./contentsSlice";
+import assessmentsReducer from "./assessmentSlice";
+import gradesReducer from "./gradesSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +15,10 @@ export const store = configureStore({
     notifications: notificationReducer,
     user: userReducer,
     events: eventsReducer,
+    subjects: subjectsReducer,
+    professors: professorsReducer,
+    contents: contentsReducer,
+    assessments: assessmentsReducer,
+    grades: gradesReducer,
   },
 });
